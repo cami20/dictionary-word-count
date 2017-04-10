41c774d398992635936b36782e72c75f8c266095
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 12:53:33 -0700

    Edited for twain.txt

--- wordcount.py
+++ wordcount.py
@@ -4,18 +4,24 @@
  # for loop that iterates over text file and adds to dictionary with counts
  # somewhere print it out the dictionary both keys and values
 
+import string
+
 def get_word_count(text_file):
 
     word_count = {}
+    exclude = []
+    exclude = set(string.punctuation)
+    exclude = exclude - {"-", "'", "."}
 
     for line in text_file:
         line = line.rstrip()
-        line = line.strip(".")
-        line = line.strip(",")
-        line = line.strip("?")
         word_list = line.split(" ")
 
         for word in word_list:
+            word = word.lower()
+            word = ''.join(ch for ch in word if ch not in exclude)
+            word = word.rstrip(".")
+            word = word.lstrip("'")
             word_count[word] = word_count.get(word, 0) + 1
 
     # print word_count
@@ -27,7 +33,7 @@ def print_word_count(word_count):
         print "%s: %d" % (word, count)
 
 
-file_text = open("test.txt")
+file_text = open("twain.txt")
 # get_word_count(file_text)
 print_word_count(get_word_count(file_text))
 
